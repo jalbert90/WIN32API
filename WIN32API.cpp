1fc355565f7d7a84c409c07ff24b03285ebda22d
jacob <jalbert90@gmail.com>
2022-11-23 23:11:58 -0500

    sketchy window

--- WIN32API.cpp
+++ WIN32API.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) 
 {
 	const wchar_t CLASS_NAME[] = L"My First Window Class";
@@ -12,8 +14,44 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
 	RegisterClass(&wc);
 
-	//Create a new instance of a window.
+	// Create a new instance of a window.
+	HWND hwnd = CreateWindowEx(
+		0,									// Optional window styles
+		CLASS_NAME,							// Window class
+		L"My first window instance!",		// Window text
+		WS_OVERLAPPEDWINDOW,				// Window style
 
+		// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+		NULL,			// Parent
+		NULL,			// Menu
+		hInstance,		// Instance handle
+		NULL			// Additional app data
+	);
+
+	if (hwnd == NULL) {
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0) > 0) 
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		break;
+	}
 }
